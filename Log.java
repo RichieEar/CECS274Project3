6e8106c2f7b620533005268dcfda42cdd2ee9d55
CrabBucket <tom@mcratracing.com>
2018-10-11 16:32:45 -0700

    Setup stuff

---
+++ Log.java
@@ -0,0 +1,10 @@
+/**
+ * Has an arraylist of records
+ * Displays them
+ * Keeps track of duplicates
+ * @author Thomas
+ *
+ */
+public class Log {
+	ArrayList<Record> log = new ArrayList<Record>();
+}
--- Record.java
+++ Record.java
@@ -6,5 +6,7 @@
  *
  */
 public class Record {
-
+	String id;
+	String date;
+	boolean IO;
 }
