17bb75d5f634eacae510c721cebf285aa8c501c5
CrabBucket <tom@mcratracing.com>
2018-10-11 19:02:59 -0700

    Update Record.java

--- Record.java
+++ Record.java
@@ -8,5 +8,30 @@
 public class Record {
 	String id;
 	String date;
-	boolean IO;
+	String IO;
+	/**
+	 * Constructor that sets all instance variables
+	 * @param id - Name or number
+	 * @param date - Date and time of call
+	 * @param IO -Incoming or outgoing call.
+	 */
+	public Record(String id, String date, String IO) {
+		this.id=id;
+		this.date = date
+		this.IO=IO;
+	}
+	/**
+	 * returns this record's id
+	 * @return id of this record
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * Returns formated string of the record.
+	 * @return - formated string
+	 */
+	public String toString() {
+		return String.format("%s %n%-20s%s", id,date,IO);
+	}
 }
